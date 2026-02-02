c11754f1169dbddc09194de6e55345ac037184d7
sarang456 <pravinbhaibarot3@gmail.com>
2026-02-02 12:29:32 +0530

    connect Django files to PostGresSQL

--- Django/learning26/learning26/settings.py
+++ Django/learning26/learning26/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'learning26.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE':'django.db.backends.postgresql',
+       'NAME':'Learning_26',
+       'USER':'postgres',
+       'PASSWORD':'1829',
+       'HOST':'localhost',
+       'PORT':'5432',
     }
 }
 
