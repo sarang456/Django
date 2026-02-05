10dcf6d44f081dd3fe8359a20bee6118fa516484
sarang456 <pravinbhaibarot3@gmail.com>
2026-02-05 11:12:12 +0530

    Create student models and register them in admin

--- Django/learning26/student/admin.py
+++ Django/learning26/student/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Student, product, internship, Course
 
-# Register your models here.
+
+admin.site.register(Student)
+admin.site.register(product)
+admin.site.register(internship)
+admin.site.register(Course)
--- Django/learning26/student/models.py
+++ Django/learning26/student/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    student_name = models.CharField(max_length=100)
+    student_Enroll = models.CharField(max_length=15)
+    student_age = models.PositiveIntegerField()
+    student_Email = models.EmailField(null=True)
+
+    class Meta:
+        db_table = "student"
+
+
+class product(models.Model):
+    Product_name = models.CharField(max_length=100)
+    Product_color = models.CharField(max_length=100)
+    Product_price = models.IntegerField()
+    Product_stock = models.PositiveIntegerField()
+    
+    class meta:
+        db_table = "product"
+class internship(models.Model):
+    company_name = models.CharField(max_length=50)
+    internship_role = models.CharField(max_length=50)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    status = models.CharField(max_length=50)
+
+    class meta:
+        db_table = "internship"
+
+class Course(models.Model):
+    cource_name = models.CharField(max_length=50)
+    Course_code = models.CharField(max_length=50)
+    Duration = models.IntegerField()
+    Fees = models.IntegerField()
+    Is_active = models.BooleanField(default=True)
+
+    class meata:
+        db_table = "course"
